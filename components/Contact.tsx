17b0c609245459b069472e1583514b8874b9ed33
mrphaox <oscar.neira@usantoto.edu.co>
2023-12-30 17:41:48 -0500

    v-5.1

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
           Siempre estoy abierto a discutir su proyecto, mejorar su presencia en
           línea, o ayudar con el diseño de su sitio web y convertir desafíos.
         </p>
-        <a href="mailto:noor.jsdivs@gmail.com">
+        <a href="mailto:oscarne09@gmail.com">
           <button className="w-52 h-14 bg-gradient-to-r from-blue-600 via-blue-700 to-indigo-950 text-xl font-bold text-white rounded-lg hover:from-indigo-950 hover:to-blue-600 duration-300">
             contáctame
           </button>
