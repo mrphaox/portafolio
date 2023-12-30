9e9a5f4ea347cf56a624d836e2bc96bbb773c114
mrphaox <oscar.neira@usantoto.edu.co>
2023-12-30 17:38:52 -0500

    v-5.1 es

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,11 +5,10 @@ const Contact = () => {
   return (
     <div className="wrapper">
       <div className="max-w-5xl mx-auto flex flex-col gap-4 items-center justify-center">
-        <h2 className="text-2xl font-bold">Let us create awesome products!</h2>
+        <h2 className="text-2xl font-bold">¡Déjame crear tus productos!</h2>
         <p className="text-lg tracking-wide font-medium text-center">
-          I am always open to discussing your project, improving your online
-          presence, or helping with Your website design and converting
-          challenges.
+          Siempre estoy abierto a discutir su proyecto, mejorar su presencia en
+          línea, o ayudar con el diseño de su sitio web y convertir desafíos.
         </p>
         <a href="mailto:noor.jsdivs@gmail.com">
           <button className="w-52 h-14 bg-gradient-to-r from-blue-600 via-blue-700 to-indigo-950 text-xl font-bold text-white rounded-lg hover:from-indigo-950 hover:to-blue-600 duration-300">
@@ -19,7 +18,10 @@ const Contact = () => {
       </div>
       <div className="mt-8 flex items-center flex-wrap   gap-8 justify-center">
         <SocialLink title="Github" link="https://github.com/mrphaox" />
-        <SocialLink title="Youtube" link="https://www.youtube.com/channel/UCw7rSN4Igb-FOyuBq9QGang" />
+        <SocialLink
+          title="Youtube"
+          link="https://www.youtube.com/channel/UCw7rSN4Igb-FOyuBq9QGang"
+        />
         <SocialLink
           title="Linkedin"
           link="https://www.linkedin.com/in/oscar-alberto-neira-vaca-75b387259/"
@@ -28,7 +30,10 @@ const Contact = () => {
           title="Facebook"
           link="https://www.facebook.com/oscar.neira0/"
         />
-        <SocialLink title="Nextjs" link="https://vercel.com/devphaoxs-projects" />
+        <SocialLink
+          title="Nextjs"
+          link="https://vercel.com/devphaoxs-projects"
+        />
       </div>
     </div>
   );
